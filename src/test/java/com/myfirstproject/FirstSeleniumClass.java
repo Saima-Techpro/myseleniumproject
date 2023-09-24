b27f28e22c814d50989fd269fc2d7db2d9fb2f11
Saima <saima@Syeds-iMac.local>
2023-09-24 09:53:22 +0400

    first commit

---
+++ src/test/java/com/myfirstproject/FirstSeleniumClass.java
@@ -0,0 +1,23 @@
+package com.myfirstproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstSeleniumClass {
+
+    public static void main(String[] args) {
+
+
+        // 1. Set the path of the driver
+        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver"); // MAC
+        // System.setProperty("webdriver.firefox.driver", "./drivers/chromedriver"); // MAC
+        // System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe"); // WINDOWS
+
+        // 2. Create the driver
+        WebDriver driver = new ChromeDriver();
+      //  WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.google.com/");
+    }
+}
