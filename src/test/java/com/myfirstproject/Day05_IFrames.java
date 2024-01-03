d31b8246c822dfea048527e937e89497ff832093
Saima <saima@Syeds-iMac.local>
2024-01-03 10:37:02 +0400

    day05 update

--- src/test/java/com/myfirstproject/Day05_IFrames.java
+++ src/test/java/com/myfirstproject/Day05_IFrames.java
@@ -15,26 +15,36 @@ public class Day05_IFrames extends TestBase {
         //    Then Assert that "Left List Item 29" is the last element in the "Left"
         //"Left List Item 29" IS INSIDE THE IFRAME SO SWITCH TO THAT FRAME FIRST
         /*
-        NOTE 1 : there are 3 ways to switch to a frame: index, id/name, webelement
+        NOTE 1 : there are 3 ways to switch to a frame: index, id/name, webElement
+                 index is not safe option to use because if developer adds any frame on webpage later on, it will change the existing indexes
         1. driver.switchTo().frame(1);
         2. driver.switchTo().frame("left");
         3. driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='left']")));
+
         NOTE 2 : when you want to switch to non-child such as sibling frames, we should go to parent then the child.
         we can not jump directly to siblings
+        //li[last()] shows 3 elements on the page when we are inspecting, but assertion doesn't see the other 2 webElements
+        // because driver is inside the first frame. It can't see the elements in the other frames
 
          */
         driver.switchTo().frame("left");
 //        NOW WE ARE INSIDE THIS IFRAME
+//        //li[@id='left29'] => is hard coded xpath. what if the last element changes/added a new one? 'left29' will not work in that case
+//        so either we create a loop or use last() function  .. there's no first() ... but we have last()-2 or last()-5 etc.
 //                //li[last()]  => XPATH RETURNS THE LAST list ELEMENT
         String lastElement = driver.findElement(By.xpath("//li[last()]")).getText();
-        System.out.println("LAST ELEMENT : "+lastElement);
+        System.out.println("Last Element : "+lastElement);
         Assertions.assertTrue(lastElement.equals("Left List Item 29"));
+
+
 //    And Assert that "Middle List Item 39" is the last element in the "Middle"
 //        to Go to sibling frames, switch to the parent first, then switch to the child
-//        driver.switchTo().parentFrame();
+//        driver.switchTo().parentFrame(); OR
         driver.switchTo().defaultContent();//switching to the main html page
         driver.switchTo().frame("middle");//switching to the middle frame
-        String middleLastItem = driver.findElement(By.xpath("//li[last()]")).getText();//driver is in the middle frame and getting the middle frame elements
-        Assertions.assertTrue(middleLastItem.equals("Middle List Item 39"));
+        String middleFrameLastItem = driver.findElement(By.xpath("//li[last()]")).getText();//driver is in the middle frame and getting the middle frame elements
+        System.out.println("Miiddle Frame LastItem = " + middleFrameLastItem);
+        Assertions.assertTrue(middleFrameLastItem.equals("Middle List Item 39"));
+
     }
 }
--- src/test/java/com/myfirstproject/Day05_Windows.java
+++ src/test/java/com/myfirstproject/Day05_Windows.java
@@ -14,17 +14,20 @@ public class Day05_Windows extends TestBase {
         driver.get("https://the-internet.herokuapp.com/windows");
 //        Assert if the window 1 title equals “The Internet”
         Assertions.assertTrue(driver.getTitle().equals("The Internet"));
+        Thread.sleep(2000);
 //        Click on the link
-        driver.findElement(By.xpath("//a[.='Click Here']")).click();
+        driver.findElement(By.linkText("Click Here")).click();
+//        driver.findElement(By.xpath("//a[.='Click Here']")).click();
 //        Assert if the window 2 title equals “New Window”
         /*
-        NOTE: we must switch window 2 to get window to tile
+        NOTE: we must switch window 2 to get window 2 title
          */
         String window1Handle = driver.getWindowHandle();
         System.out.println("Window 1: "+window1Handle);
-        Set<String> allWindowHandles = driver.getWindowHandles();
+        Set<String> allWindowHandles = driver.getWindowHandles(); // returns all windows handles
         System.out.println("All Windows: "+allWindowHandles);
 
+//       Use for each loop to get the window2 handle so we can switch to it
         for (String eachHandle : allWindowHandles){
             if (!eachHandle.equals(window1Handle)){
                 driver.switchTo().window(eachHandle);
@@ -35,10 +38,12 @@ public class Day05_Windows extends TestBase {
         Thread.sleep(3000);
         Assertions.assertTrue(driver.getTitle().equals("New Window"));
         String window2Handle = driver.getWindowHandle();
+        System.out.println("window2Handle = " + window2Handle);
 //        Switch back to window 1 title and assert if URL contains “windows”
         driver.switchTo().window(window1Handle);
         Assertions.assertTrue(driver.getCurrentUrl().contains("windows"));
 //        And assert if the URL doesn’t contain ‘new’
+//        Assertions.assertTrue(!driver.getCurrentUrl().contains("new"));
         Assertions.assertFalse(driver.getCurrentUrl().contains("new"));
 
 //        Switch back to window 2
